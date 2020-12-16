fbff98c4229b5748a6fa4794d7b1ee2483f2c5ea
Leandro Villalobos <villalobosleandro1@gmail.com>
2020-12-15 22:35:10 -0400

    se implemento el carrito de compras y se guarda en el localstorage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
-
+import Cart from './components/Cart';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")): [],
       size: "",
       sort: ""
     };
@@ -51,6 +52,37 @@ class App extends React.Component {
     }
   }
 
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item => {
+      if(item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if(!alreadyInCart) {
+      cartItems.push({...product, count: 1});
+    }
+
+    this.setState({
+      cartItems
+    });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id)
+    })
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x) => x._id !== product._id)));
+  }
+
+  createOrder = (order) => {
+    alert("Need to save order for " + order.name);
+  }
+
   render() {
     return (
       <div className="grid-container">
@@ -68,9 +100,18 @@ class App extends React.Component {
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
               ></Filter>
-              <Products products={this.state.products}></Products>
+              <Products 
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
             </div>
-            <div className="sidebar">cart items</div>
+            <div className="sidebar">
+              <Cart 
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
+              />
+            </div>
           </div>
         </main>
   
